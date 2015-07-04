fbe2df7b302dc407bddfa50c8821a841e360e72a
soba1104 <shiba@rvm.jp>
2015-07-04 20:23:19 +0900

    リファクタ

--- public/javascripts/stats/author.js
+++ public/javascripts/stats/author.js
@@ -2,8 +2,5 @@ function Author(name, activity) {
   return {
     name: name,
     activity: activity,
-    span: function(span) {
-      return (new Author(this.name, this.activity.span(span)))
-    }
   }
 }
