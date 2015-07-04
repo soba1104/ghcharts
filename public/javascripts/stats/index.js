7322c783a0f9dde8f7261cb885d6d609e3b63429
soba1104 <shiba@rvm.jp>
2015-07-04 19:03:08 +0900

    グラフの描画に使う色の候補をいくつか用意する

--- public/javascripts/stats/index.js
+++ public/javascripts/stats/index.js
@@ -8,6 +8,22 @@ $(document).ready(function() {
     },
   })
 
+  var colors = [
+    'rgba(255, 0, 0, 1)',
+    'rgba(0, 255, 0, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(255, 255, 0, 1)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(255, 0, 255, 1)',
+    'rgba(192, 192, 192, 1)',
+    'rgba(128, 0, 0, 1)',
+    'rgba(0, 128, 0, 1)',
+    'rgba(0, 0, 128, 1)',
+    'rgba(128, 128, 0, 1)',
+    'rgba(0, 128, 128, 1)',
+    'rgba(128, 0, 128, 1)',
+  ]
+
   stats.onload = function() {
     var limit = 10
     var stat = stats.get(0)
@@ -15,7 +31,7 @@ $(document).ready(function() {
     var labels = stat.dates.slice(-limit)
     var adds = stat.adds.slice(-limit)
     var dels = stat.dels.slice(-limit)
-    var color = 'rgba(255, 228, 225, 1)'
+    var color = colors[0]
     var datasets = [
       {
         fillColor: 'rgba(255, 255, 255, 0)',
