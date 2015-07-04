e243828af4405e2a75977c5fcb4c7ed4049b0f2d
soba1104 <shiba@rvm.jp>
2015-07-04 16:57:39 +0900

    直近１０件の結果だけを表示する

--- public/javascripts/stats/index.js
+++ public/javascripts/stats/index.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
   var stats = new Stats($(location).attr('href') + '/json')
   stats.onload = function() {
-    var labels = stats.dates
+    var limit = 10
+    var labels = stats.dates.slice(-limit)
+    var adds = stats.adds.slice(-limit)
+    var dels = stats.dels.slice(-limit)
     var datasets = [
       {
         fillColor: 'rgba(255, 255, 255, 0)',
         strokeColor: 'rgba(255, 228, 225, 1)',
         pointColor: 'rgba(255, 228, 225, 1)',
         pointStrokeColor: '#fff',
-        data: stats.adds,
+        data: adds,
       },
       {
         fillColor: 'rgba(255, 255, 255, 0)',
         strokeColor: 'rgba(230, 230, 250, 1)',
         pointColor: 'rgba(230, 230, 250, 1)',
         pointStrokeColor: '#fff',
-        data: stats.dels,
+        data: dels,
       },
     ]
     new Chart($('#chart').get(0).getContext('2d')).Line({
