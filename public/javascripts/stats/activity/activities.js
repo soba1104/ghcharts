4413c1dfcee413be45f72309801f59e53fbf057f
soba1104 <shiba@rvm.jp>
2015-07-09 13:28:14 +0900

    リファクタ

--- public/javascripts/stats/activity/activities.js
+++ public/javascripts/stats/activity/activities.js
@@ -1,20 +1,24 @@
-function Activities(data) {
+function Activities(d) {
   return {
-    data: data,
-    filter_by_span: function(span) {
+    data: d,
+    dates: function() {
       var data = this.data
       var dates_h = {}
       for (var i = 0; i < data.length; i++) {
         var act = data[i]
-        dates_h[act.date] = true
+          dates_h[act.date] = true
       }
       var dates_a = Object.keys(dates_h)
-      var dates = dates_a.sort().slice(-span)
+      return dates_a.sort()
+    },
+    filter_by_span: function(span) {
+      var data = this.data
+      var dates = this.dates().slice(-span)
       var s = dates[0]
       var e = dates[dates.length - 1]
       return new Activities(data.filter(function(act) {
         return s <= act.date && act.date <= e
       }))
-    }
+    },
   }
 }
