1a97895a6095479076f3598fb60775c69c135c58
soba1104 <shiba@rvm.jp>
2015-07-12 12:55:58 +0900

    chart の表示に必要なデータを生成する

--- public/javascripts/stats/activity/boot.js
+++ public/javascripts/stats/activity/boot.js
@@ -1,13 +1,74 @@
 $(document).ready(function() {
   var loader = new Loader()
   loader.onload = function(activities) {
-    activities = activities.filter_by_span(10)
-    activities = activities.filter_by_user_abs(10)
-    var dates = activities.dates()
-    var users = activities.users()
-    console.log(activities.data)
-    console.log(activities.users())
-    console.log(activities.group_by_date())
+    var colors = [
+      'rgba(255, 0, 0, 1)',
+      'rgba(0, 255, 0, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(255, 255, 0, 1)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(255, 0, 255, 1)',
+      'rgba(192, 192, 192, 1)',
+      'rgba(128, 0, 0, 1)',
+      'rgba(0, 128, 0, 1)',
+      'rgba(0, 0, 128, 1)',
+      'rgba(128, 128, 0, 1)',
+      'rgba(0, 128, 128, 1)',
+      'rgba(128, 0, 128, 1)',
+    ]
+    var num = colors.length
+    activities = activities.filter_by_span(num)
+    activities = activities.filter_by_user_abs(num)
+    var dates_a = activities.dates()
+    var users_h = activities.group_by_user()
+    dates_a = dates_a.sort(function(d0, d1) {
+      return d0 > d1 ? 1 : -1;
+    })
+    var datasets = []
+    var legends = []
+    for (var user in users_h) {
+      var acts = users_h[user]
+      var dates_h = {}
+      for (var i = 0; i < acts.length; i++) {
+        var act = acts[i]
+        dates_h[act.date] = act
+      }
+      var adds = []
+      var dels = []
+      for (var i = 0; i < dates_a.length; i++) {
+        var date = dates_a[i]
+        var act = dates_h[date]
+        if (act) {
+          adds.push(act.add)
+          dels.push(act.del)
+        } else {
+          adds.push(0)
+          dels.push(0)
+        }
+      }
+      var color = colors[legends.length]
+      datasets.push({
+        fillColor: 'rgba(255, 255, 255, 0)',
+        strokeColor: color,
+        pointColor: color,
+        pointStrokeColor: '#fff',
+        data: adds,
+      })
+      datasets.push({
+        fillColor: 'rgba(255, 255, 255, 0)',
+        strokeColor: color,
+        pointColor: color,
+        pointStrokeColor: '#fff',
+        data: dels,
+      })
+      legends.push({
+        user: user,
+        color: color,
+      })
+    }
+    var labels = dates_a
+    console.log(labels)
+    console.log(datasets)
   }
   loader.load($(location).attr('href') + '/json')
 })
